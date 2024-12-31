54933ce53d25499b0eb899864ac74d04b8f59d75
Wix-Code <ogbonna428alex@gmail.com>
2024-12-31 10:26:52 +0100

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-[100vh] bg-from-stone-800 to-slate-950">
+      <div>
+        <h1>Don't have CV</h1>
+      </div>
+    </div>
+  );
+}
